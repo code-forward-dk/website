072fe0536b28e01066d3deddd27c2730cf06faf2
Bence A. Tóth <tothab@gmail.com>
2023-04-18 20:47:52 +0200

    Deleted some sections

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,7 @@
 import Splash from "./Splash";
 import Mission from "./Mission";
-import Manifesto from "./Manifesto";
-import Apply from "./Apply";
-import Activities from "./Activities";
 import Curriculum from "./Curriculum";
-import News from "./News";
 import Team from "./Team";
-import Join from "./Join";
 import Donate from "./Donate";
 import Contact from "./Contact";
 
@@ -14,14 +9,9 @@ const Main = () => (
   <main className="site-main">
     <Splash />
     <Mission />
-    <Manifesto />
-    <Apply />
-    <Activities />
     <Curriculum />
-    <News />
-    <Donate />
     <Team />
-    <Join />
+    <Donate />
     <Contact />
   </main>
 );
